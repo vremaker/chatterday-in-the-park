4bf3178f1d4ba3d9275155d8c6250b2d158680b6
Valerie Remaker <44219004+vremaker@users.noreply.github.com>
2020-07-07 22:17:07 -0700

    yeehaw

--- src/App.js
+++ src/App.js
@@ -3,20 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import Footer from './footer';
 import MessageSend from './message-send';
+import {initialize, useDatu} from 'datu';
 
 function App() {
-  const [messages,setMessages] = useState([])
+  const {messages, send} = useDatu()
+  console.log(messages)
   return (
-    <main class="main">
+    <main className="main">
   <header>
     <span>Chatty Patty </span>
   </header>
-  {messages.map((m, i) => <MessageSend key={i} text ={m} />)}
-  <Footer onSend = {(text)=> setMessages([...messages,text])} />
+  <div className="messages">
+  {messages.map((m, i) => <MessageSend key={i} text ={m.text} />)}
+ 
+  </div>
+  <Footer onSend = {text => send({text:text})} />
 </main>
 
 
-  )
+  );
 }
 
 export default App;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWXVgUqm3xATyzqUqTxcpvsW7U804ctXI",
+  authDomain: "chatter20202020.firebaseapp.com",
+  databaseURL: "https://chatter20202020.firebaseio.com",
+  projectId: "chatter20202020",
+  storageBucket: "chatter20202020.appspot.com",
+  messagingSenderId: "630230183323",
+  appId: "1:630230183323:web:cc967f51fc79e394ca053e"
+};
+
+initialize(firebaseConfig)
